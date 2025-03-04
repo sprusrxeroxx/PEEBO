c3d8cef5485c7e3d4bd5060da0d8508f79ec249b
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-04 17:50:03 +0000

    launched express server and landing page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config()
-
+import 'dotenv/config.js';
 
 const app = express();
 
-app.listen(process.env.PO, () =>{
-    console.log(`Server is running on Port ${PORT}`)
+const PORT = process.env.PORT || 5000;
+
+app.listen(process.env.PORT, (error) => {
+    if (!error) { 
+        console.log(`Server is running on Port ${PORT}`)
+    }
+    else {
+        console.log(`Something went wrong on the server`, error)
+    }
+})
+
+app.use('/', (req, res) => {
+    res.status(200).send('Welcome to the Peebo store')
 })

228f468c2814a916ceaa1defc4aededda9b2b7ed
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-09 18:39:06 +0000

    bug: fixed dns error for incorrect password for db user

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
+import "dotenv/config.js";
 import express from "express";
-import 'dotenv/config.js';
+import mongoose from "mongoose";
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(process.env.PORT, (error) => {
+mongoose.connect(MONGO_URI);
+
+app.listen(PORT, (error) => {
     if (!error) { 
-        console.log(`Server is running on Port ${PORT}`)
+        console.log(`Server is running on Port ${PORT}`);
+        console.log(`Successfully ${MONGO_URI}`)
     }
     else {
         console.log(`Something went wrong on the server`, error)

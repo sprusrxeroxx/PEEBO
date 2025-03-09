44c80014cf261cb4aeb0cff29645f33c41869bad
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-09 20:01:26 +0000

    chore: added schema for product model

---
+++ server/models/products.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const productSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      category: {
+        type: String,
+        enum: ['Fruits', 'Vegetables', 'Other'],
+        required: true,
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+      unit: {
+        type: String,
+        enum: ['kg', 'piece', 'bunch', 'pack', 'grams'],
+        required: true,
+      },
+      stockQuantity: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0,
+      },
+      imageUrl: {
+        type: String,
+        trim: true,
+      },
+      seasonal: {
+          type:Boolean,
+          default: false,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      },
+      origin: {
+        type: String,
+        trim: true,
+      },
+})
+
+const Product = model('Product', productSchema);
+
+export default Product;

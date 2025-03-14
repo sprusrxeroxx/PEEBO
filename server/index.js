572a3849c976c2327895590d5adfbd6adfd73fa7
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-14 04:03:43 +0000

    feat: Added CRUD endpoints

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import "dotenv/config.js";
 import express from "express";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.listen(PORT, (error) => {
     if (!error) { 
@@ -16,6 +20,79 @@ app.listen(PORT, (error) => {
     }
 })
 
-app.get('/products', (req, res) => {
-    res.status(200).send('Welcome to the Peebo store')
+app.get('/api/v1/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success:true, data: products });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in fetching products' })
+    }
+});
+
+app.get('/api/v1/products/:id', async (req, res) => {
+    const { id } = req.params;
+
+    if (!id){
+        return res.status(400).json({ success:false, message: "Please specify product"})
+    }
+    try {
+        const product = await Product.findOne({ _id: id });
+        res.status(200).json({ success:true, data: product });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in fetching product' })
+    }
 })
+
+app.post('/api/v1/products', async (req, res) => {
+    const product = req.body;
+
+    if ( !product.name || !product.category || !product.price || !product.unit || !product.stockQuantity || !product.imageUrl ) {
+        return res.status(400).json({ success:false, message: "Please fill in all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success:true, data: newProduct});
+    } catch (error) {
+        console.log('Error in Create product:', error.message);
+        res.status(500).json({ success:false, message: 'Server Error' })
+    }
+})
+
+app.put('/api/v1/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success:false, message: "Invalid Product Id"})
+    };
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, update, {new:true});
+        res.status(204).json({ success:true, data: updatedProduct });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in updating product' });
+    }
+})
+
+app.delete('/api/v1/products/:id', async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success:false, message: "Invalid Product Id"})
+    };
+
+    try {
+        const updatedProduct = await Product.findByIdAndDelete(id, {new:true});
+        res.status(202).json({ success:true, data: updatedProduct });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in deleting product' });
+    }
+})
+

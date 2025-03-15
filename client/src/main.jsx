24a1fa8c00bd66d47ce20f93995610f121009719
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-15 17:40:31 +0000

    build: finished setup for react client + downgraded react/comps for compatabilit

---
+++ client/src/main.jsx
@@ -0,0 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+)

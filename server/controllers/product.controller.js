f59b401ee933fa204124931a7bb0421ddad404c2
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-03-15 03:56:20 +0000

    refactor: Added controller module for each endpoint

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,79 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success:true, data: products });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in fetching products' })
+    }
+}
+
+export const getProduct = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success:false, message: "Invalid Product Id"})
+    };
+    
+    try {
+        const product = await Product.findOne({ _id: id });
+        res.status(200).json({ success:true, data: product });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in fetching product' })
+    }
+}
+
+export const createProduct = async (req, res) => {
+    const product = req.body;
+
+    if ( !product.name || !product.category || !product.price || !product.unit || !product.stockQuantity || !product.imageUrl ) {
+        return res.status(400).json({ success:false, message: "Please fill in all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success:true, data: newProduct});
+    } catch (error) {
+        console.log('Error in Create product:', error.message);
+        res.status(500).json({ success:false, message: 'Server Error' })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success:false, message: "Invalid Product Id"})
+    };
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, update, {new:true});
+        res.status(204).json({ success:true, data: updatedProduct });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in updating product' });
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success:false, message: "Invalid Product Id"})
+    };
+
+    try {
+        const updatedProduct = await Product.findByIdAndDelete(id, {new:true});
+        res.status(202).json({ success:true, data: updatedProduct });
+    } catch (error) {
+        console.log('Error in Fetching products:', error.message);
+        res.status(500).json({ success:false, message: 'Error in deleting product' });
+    }
+}
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,85 +1,17 @@
 import express from "express";
-import mongoose from "mongoose";
-import Product from "../models/product.model.js"
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success:true, data: products });
-    } catch (error) {
-        console.log('Error in Fetching products:', error.message);
-        res.status(500).json({ success:false, message: 'Error in fetching products' })
-    }
-});
+router.get('/', getProducts);
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
+router.get('/:id', getProduct);
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({ success:false, message: "Invalid Product Id"})
-    };
-    
-    try {
-        const product = await Product.findOne({ _id: id });
-        res.status(200).json({ success:true, data: product });
-    } catch (error) {
-        console.log('Error in Fetching products:', error.message);
-        res.status(500).json({ success:false, message: 'Error in fetching product' })
-    }
-})
+router.post('/', createProduct)
 
-router.post('/', async (req, res) => {
-    const product = req.body;
+router.put('/:id', updateProduct)
 
-    if ( !product.name || !product.category || !product.price || !product.unit || !product.stockQuantity || !product.imageUrl ) {
-        return res.status(400).json({ success:false, message: "Please fill in all fields" });
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success:true, data: newProduct});
-    } catch (error) {
-        console.log('Error in Create product:', error.message);
-        res.status(500).json({ success:false, message: 'Server Error' })
-    }
-})
-
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const update = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({ success:false, message: "Invalid Product Id"})
-    };
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, update, {new:true});
-        res.status(204).json({ success:true, data: updatedProduct });
-    } catch (error) {
-        console.log('Error in Fetching products:', error.message);
-        res.status(500).json({ success:false, message: 'Error in updating product' });
-    }
-})
-
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({ success:false, message: "Invalid Product Id"})
-    };
-
-    try {
-        const updatedProduct = await Product.findByIdAndDelete(id, {new:true});
-        res.status(202).json({ success:true, data: updatedProduct });
-    } catch (error) {
-        console.log('Error in Fetching products:', error.message);
-        res.status(500).json({ success:false, message: 'Error in deleting product' });
-    }
-})
+router.delete('/:id', deleteProduct)
 
 export default router;

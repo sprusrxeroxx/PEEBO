6b31db24b73940af57973a76c1cf290a9611d22a
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-04-20 15:22:41 +0000

    fix: corrected spelling error

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -99,7 +99,7 @@ const HomePage = () => {
                 ))}
               </SimpleGrid>
 
-            {products.lenth === 0 && (<Text fontSize="xl" textAlign={"center"} fontWeight={"bold"} color='gray.500'>
+            {products.length === 0 && (<Text fontSize="xl" textAlign={"center"} fontWeight={"bold"} color='gray.500'>
               No Products Found {" "}
               <Link to={'/create'}>
                 <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>

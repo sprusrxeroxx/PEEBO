8a6d1e62e5a35cc2b2e255865d0b2575632a1ecf
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-04-26 14:14:29 +0000

    feat: handled form submition and global state for recipe endpoint using zustand

---
+++ client/src/store/recipe.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+
+export const useRecipeStore = create((set) => ({
+  recipes: [], // Store for fetched recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  fetchRecipes: async (ingredients) => {
+    try {
+      const res = await fetch(`/api/v1/recipes/search?ingredients=${ingredients}`);
+      const data = await res.json();
+
+      if (data.success) {
+        set({ recipes: data.data }); // Update the global state with fetched recipes
+        return { success: true };
+      } else {
+        return { success: false, message: data.message };
+      }
+    } catch (error) {
+      console.error("Error fetching recipes:", error.message);
+      return { success: false, message: "Failed to fetch recipes." };
+    }
+  },
+}));

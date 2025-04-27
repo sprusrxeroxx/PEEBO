c7beca9042d5e673ef26bfd76f6b5da8dbfa5523
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-04-27 00:20:10 +0000

    chore: added logout to auth state handler

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -18,6 +18,10 @@ export function AuthProvider({ children }) {
         return auth.signInWithEmailAndPassword(email, password)
     }
 
+    function logout() {
+        return auth.signOut()
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -30,6 +34,7 @@ export function AuthProvider({ children }) {
         currentUser,
         signup,
         login,
+        logout,
     }
 
     return (

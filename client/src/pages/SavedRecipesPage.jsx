ee4c2348a281c4f215cb5505f966ea0202fb3310
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-04-29 03:28:21 +0000

    feat: added page to view saved recipes

---
+++ client/src/pages/SavedRecipesPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@chakra-ui/react";
+import SavedRecipes from "../components/SavedRecipes";
+
+const SavedRecipesPage = () => {
+  return (
+    <Container maxW="container.xl" py={8}>
+      <SavedRecipes />
+    </Container>
+  );
+};
+
+export default SavedRecipesPage;

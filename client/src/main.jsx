e475d74814e3706cc8494d22cd83257cd0a4a06d
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-04-29 22:53:06 +0000

    fix: commented out fonts for testing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext.jsx'; // Import AuthProvider
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import theme from './theme/theme.js';
+// import '@fontsource/montserrat/400.css';
+// import '@fontsource/montserrat/600.css';
+// import '@fontsource/montserrat/700.css';
+// import '@fontsource/open-sans/400.css';
+// import '@fontsource/open-sans/600.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider> {/* Wrap the app with AuthProvider */}
-        <ChakraProvider>
+      <AuthProvider>
+        <ChakraProvider theme={theme}>
           <App />
         </ChakraProvider>
       </AuthProvider>

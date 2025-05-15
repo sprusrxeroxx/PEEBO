72880cc422ce4d2e8bf186e962c52213348623c7
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-05-15 16:31:46 +0000

    feat: add CORS middleware for handling cross-origin requests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config.js";
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from './routes/product.route.js';
 import recipeRoutes from "./routes/recipe.route.js";
@@ -8,7 +9,17 @@ import userRoutes from "./routes/user.route.js";
 
 const app = express();
 
+// Security Middleware
 app.use(helmet());
+
+// CORS Middleware
+app.use(cors({
+    origin: process.env.NODE_ENV === "production"
+    ? process.env.FRONTEND_URL
+    : "http://localhost:5173",
+    credentials: true,
+}));
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
